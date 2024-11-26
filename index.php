be48a1b6b1802aab9791b3a51f8cbd935d694ae1
holden.nguyen <holdennguyen6174@gmail.com>
2024-11-27 00:48:45 +0700

    Testimonials using SwiperJS

--- index.php
+++ index.php
@@ -393,6 +393,7 @@
     </div>
   </div>
 
+  <!-- Our Facilities -->
   <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
   <div class="container">
     <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
@@ -422,6 +423,64 @@
     </div>
   </div>
  
+  <!-- Testimonials -->
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">TESTIMONIALS</h2>
+  <div class="container">
+    <div class="swiper swiper-testimonials">
+      <div class="swiper-wrapper mb-5">
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="images/facilities/wifi.svg" width="30px" alt="">
+            <h6 class="m-0 ms-2">Random user1</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Labore qui dignissimos iste, rerum quo, magnam impedit exercitationem magni delectus eveniet in ipsum eius atque doloribus nihil corporis aliquam, nemo eum.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="images/facilities/wifi.svg" width="30px" alt="">
+            <h6 class="m-0 ms-2">Random user1</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Labore qui dignissimos iste, rerum quo, magnam impedit exercitationem magni delectus eveniet in ipsum eius atque doloribus nihil corporis aliquam, nemo eum.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+        <div class="swiper-slide bg-white p-4">
+          <div class="profile d-flex align-items-center mb-3">
+            <img src="images/facilities/wifi.svg" width="30px" alt="">
+            <h6 class="m-0 ms-2">Random user1</h6>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Labore qui dignissimos iste, rerum quo, magnam impedit exercitationem magni delectus eveniet in ipsum eius atque doloribus nihil corporis aliquam, nemo eum.
+          </p>
+          <div class="rating">
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+            <i class="bi bi-star-fill text-warning"></i>
+          </div>
+        </div>
+
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+  </div>
 
   <br><br><br>
   <br><br><br>
@@ -436,6 +495,39 @@
       autoplay: {
         delay: 3500,
         disableOnInteraction: false,
+      }
+    });
+
+    var swiper = new Swiper(".swiper-testimonials", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      slidesPerView: "3",
+      loop: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
       }
     });
   </script>
