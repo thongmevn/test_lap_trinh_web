ddca94ea63e9ef2067c5cbc251734fe8105f6011
holden.nguyen <holdennguyen6174@gmail.com>
2024-11-27 01:30:33 +0700

    Footer Design

--- index.php
+++ index.php
@@ -524,8 +524,40 @@
     </div>
   </div>
 
-  <br><br><br>
-  <br><br><br>
+  <!-- Footer -->
+  <div class="container-fluid bg-white mt-5">
+    <div class="row">
+      <div class="col-lg-4 p-4">
+        <h3 class="h-font fw-bold fs-3 mb-2">HOTEL</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Numquam dolorum cupiditate nostrum delectus adipisci enim saepe consequuntur nobis praesentium? Soluta mollitia quod ullam sequi nulla natus voluptates reprehenderit, molestias ipsa.
+        </p>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Links</h5>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+        <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a><br>
+      </div>
+      <div class="col-lg-4 p-4">
+        <h5 class="mb-3">Follow us</h5>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-tiktok me-1"></i> Tiktok
+        </a>
+        <a href="#" class="d-inline-block text-dark text-decoration-none mb-2">
+          <i class="bi bi-facebook me-1"></i> Facebook
+        </a>
+        <a href="#" class="d-inline-block text-dark text-decoration-none">
+          <i class="bi bi-instagram me-1"></i> Instagram
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developd by Holden</h6>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
