56b26da7672919ab5ad53720078f0f49b2b00773
holden.nguyen <holdennguyen6174@gmail.com>
2024-11-27 00:33:14 +0700

    Our Facilities

--- index.php
+++ index.php
@@ -227,7 +227,7 @@
   </div>
 
   <!-- Our Rooms -->
-  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMs</h2>
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR ROOMS</h2>
   <div class="container">
     <div class="row">
       <div class="col-lg-4 col-md-6 my-3">
@@ -392,6 +392,36 @@
       </div>
     </div>
   </div>
+
+  <h2 class="mt-5 pt-4 mb-4 text-center fw-bold h-font">OUR FACILITIES</h2>
+  <div class="container">
+    <div class="row justify-content-evenly px-lg-0 px-md-0 px-5">
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px" alt="">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px" alt="">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px" alt="">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px" alt="">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-2 col-md-2 text-center bg-white rounded shadow py-4 my-3">
+        <img src="images/facilities/wifi.svg" width="80px" alt="">
+        <h5 class="mt-3">Wifi</h5>
+      </div>
+      <div class="col-lg-12 text-center mt-5">
+        <a href="#" class="btn btn-sm btn-outline-dark rounded-0 fw-bold shadow-none">More Facilities >>></a>
+      </div>
+    </div>
+  </div>
+ 
 
   <br><br><br>
   <br><br><br>
