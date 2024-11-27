be67a4782e59a6ca5cdd6f80d1d9b0f5b2592999
holden.nguyen <holdennguyen6174@gmail.com>
2024-11-28 06:38:56 +0700

    Facilities Page Design

---
+++ facilities.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hotel - FACILITIES</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+  <?php require('inc/links.php'); ?>
+  <style>
+    .pop:hover {
+			border-top-color: var(--teal) !important;
+			transform: scale(1.03);
+			transition: all 0.3s;
+		}
+  </style>
+</head>
+<body class="bg-light">
+  
+  <?php require('inc/header.php'); ?>
+
+  <div class="my-5 px-4">
+		<h2 class="fw-bold h-font text-center">OUR FACILITIES</h2>
+		<div class="h-line bg-dark"></div>
+		<p>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, asperiores omnis.
+			Recusandae, saepe quia sint labore, cum earum rem iure voluptatem id consequuntur tempore? Ratione sed hic voluptatem ab? Et!
+		</p>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-6 mb-5 px-4">
+				<div class="bg-white rounded shadow p-4 border-top border-4 border-dark pop">
+					<img src="images/facilities/wifi.svg" width="40px" alt="">
+					<h5>wifi</h5>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Neque ullam veniam quis aliquid vitae iure ipsam corporis? Delectus, eveniet voluptatibus libero quidem quae ab, debitis obcaecati nulla minus itaque deleniti.
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+  <?php require('inc/footer.php'); ?>
+
+</body>
+</html>
---
+++ inc/links.php
@@ -0,0 +1,6 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=Ubuntu:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="css/common.css">
